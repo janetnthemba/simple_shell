8410b09fbd5f3549c8cdec2c0897c7b426dcc45d
mwaskagi <gituiku.mwangi@gmail.com>
2022-12-19 03:04:17 -0800

    Betty Checks

--- main.c
+++ main.c
@@ -1,44 +1,19 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * main - Function use putchar to list alphabets
+ * Description: list alphabets to lowercase
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 on success
  */
-int main(int ac, char **av)
+int main(void)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	int ch;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
-
-	if (ac == 2)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
+		putchar(ch);
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+	putchar('\n');
+	return (0);
 }
